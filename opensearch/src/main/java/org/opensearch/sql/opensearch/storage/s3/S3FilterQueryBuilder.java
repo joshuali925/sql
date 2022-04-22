35896d3345163113693c479b815adfaf0da2fdca
Joshua Li <joshuali925@gmail.com>
2022-04-22 15:44:40 -0700

    Refactor s3 scan
    Signed-off-by: Joshua Li <joshuali925@gmail.com>

---
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/s3/S3FilterQueryBuilder.java
@@ -0,0 +1,5 @@
+package org.opensearch.sql.opensearch.storage.s3;
+
+public class S3FilterQueryBuilder {
+
+}
--- opensearch/src/main/java/org/opensearch/sql/opensearch/storage/s3/S3ObjectContent.java
+++ opensearch/src/main/java/org/opensearch/sql/opensearch/storage/s3/S3ObjectContent.java
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-package org.opensearch.sql.opensearch.s3;
+package org.opensearch.sql.opensearch.storage.s3;
 
 import java.io.BufferedReader;
 import java.io.IOException;
